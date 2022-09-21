ecbfb91cc02e2562bb9d9f43b5d21f488f7d91d1
Tolulope Amole <amoletolulope@gmail.com>
2022-09-21 18:42:50 +0100

    hero section updated

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -12,9 +12,9 @@ function Hero() {
       <Header />
       <div className="container">
         <div className="left">
-          {/* <h1>Hi!</h1>
+          <h1>Hi!</h1>
           <h1>I'm Tolulope Amole</h1>
-          <p>I'm a Full Stack Developer with more than a year of experience who is very proficient in using HTML, CSS, JavaScript, React, and Python in developing a very friendly and responsive user interface powered on any Database Management System.</p> */}
+          <p>I'm a Full Stack Developer with more than a year of experience who is very proficient in using HTML, CSS, JavaScript, React, and Python in developing a very friendly and responsive user interface powered on any Database Management System.</p>
         </div>
         <div className="right">
           <img src={ellipse} alt="ring" />
